424ac6faeecac740980785bceb4dc8ce06d97aa1
D-baj <debajbahyabaa.h@gmail.com>
2022-06-19 18:57:57 +0700

    update

--- hisoka.js
+++ hisoka.js
@@ -1879,7 +1879,7 @@ break
                 let quality = args[1] ? args[1] : '360p'
                 let media = await ytv(text, quality)
                 if (media.filesize >= 100000) return m.reply('File Melebihi Batas '+util.format(media))
-                m.reply(mess.wait)
+                replay(mess.wait)
                 hisoka.sendMessage(m.chat, { video: { url: media.dl_link }, mimetype: 'video/mp4', fileName: `${media.title}.mp4`, caption: `⭔ Title : ${media.title}\n⭔ File Size : ${media.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP3\n⭔ Resolusi : ${args[1] || '360p'}` }, { quoted: m })
             }
             break
