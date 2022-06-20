f7e4a6f19a256006bfa455c89a617773e97daa4a
D-baj <debajbahyabaa.h@gmail.com>
2022-06-21 00:39:43 +0700

    update

--- hisoka.js
+++ hisoka.js
@@ -1891,7 +1891,7 @@ break
                   mimetype: 'video/mp4',
                   contextInfo: thumbnail,
                   fileName: `${media.title}.mp4`,
-                  caption: `⭔ Title : ${media.title}\n⭔ File Size : ${media.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP3\n⭔ Resolusi : ${args[1] || '360p'}` }, { quoted: m })
+                  caption: `⭔ Title : ${media.title}\n⭔ File Size : ${media.filesizeF}\n⭔ Url : ${isUrl(text)}\n⭔ Ext : MP4\n⭔ Resolusi : ${args[1] || '360p'}` }, { quoted: m })
             }
             break
 	    case 'getmusic': {
