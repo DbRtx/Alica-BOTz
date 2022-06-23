8b74e7ddd4595ac4559da7a144edcb4a89d312fd
D-baj <debajbahyabaa.h@gmail.com>
2022-06-23 16:20:06 +0700

    update

--- hisoka.js
+++ hisoka.js
@@ -792,22 +792,20 @@ switch(command) {
     break
 
   case 'gitclone': {
-    try{
+    /*try{*/
       let regex = /(?:https|git)(?::\/\/|@)github\.com[\/:]([^\/:]+)\/(.+)/i
       if (!text) return replay( 'link githubnya mana? contoh: https://github.com/saipulanuar/v18 ')
-      if (!regex.test(text)) return replay('link salah!')
-      let [, user, repos] = text.match(regex) || []
-      let repo = repos.replace(/.git$/, '')
-      let url = `https://api.github.com/repos/${user}/${repos}/zipball`
-      let filename = text.split("/")[4]
+       if (!regex.test(text)) return replay('link salah!')
+       let [, user, repos] = text.match(regex) || []
+       let repo = repos.replace(/.git$/, '')
+       let url = `https://api.github.com/repos/${user}/${repos}/zipball`
+       let filename = (await fetch(url, {method: 'HEAD'})).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1]
 // 'attachment; filename=ilmanhdyt/ShiraoriBOT-Mdv2.5.1-251-g836cccd.zip'
        replay(mess.wait)
-       await hisoka.sendMedia(m.chat, url, m, { 
-         fileName: filename 
-       })
-       } catch (err){
+       await hisoka.sendMedia(m.chat, url, m, { fileName: filename })
+       /*} catch (err){
          replay("Error :(")
-       }
+       }*/
   }
     break
 
